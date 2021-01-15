8a603a51d603f7e6bc9b19b60171b000097768ed
LinceIberico <61143255+LinceIberico@users.noreply.github.com>
2021-01-15 19:07:59 +0100

    Update insertar.php

--- insertar.php
+++ insertar.php
@@ -4,7 +4,7 @@ $Titulo = $_GET["Titulo"];
 $Autor = $_GET["Autor"];
 $Puntuacion = $_GET["Puntuacion"];
 
-$conn = mysqli_connect("localhost","root","","biblioteca");
+$conn = mysqli_connect("localhost","root","","id15910404_biblioteca");
 $sql = "insert into libro (ISBN, Titulo, Autor, Puntuacion) values ('$ISBN','$Titulo','$Autor','$Puntuacion')";
 $esOk = mysqli_query($conn,$sql);
 	mysqli_close($conn);
